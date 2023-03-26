ee80bf48897ddac1e1b2eff2394e3494c1df656e
Greg Brandon <gebbber@gmail.com>
2023-03-26 15:16:30 -0600

    Added scrollbars; minor adjustments

--- src/App.js
+++ src/App.js
@@ -11,16 +11,33 @@ export default function App() {
     return (
         <Main>
             <AddWidgetDataContext>
-                <div className="w-full md:max-w-2xl mx-auto flex flex-col gap-4 py-2">
-                    <Header />
-                    <Instructions />
-                    <AdjustSettings />
-                    <RequestsList />
-                </div>
-                <div className="absolute bottom-0 left-0 w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-neutral-800 md:!bg-transparent dark:md:bg-vert-dark-gradient pt-2">
-                    <TextInputForm />
+                <div className="h-screen flex flex-col justify-between w-full">
+                    <div className="overflow-y-scroll grow-1">
+                        <div className="w-full md:max-w-2xl mx-auto flex flex-col gap-4 py-2 ">
+                            <Header />
+                            <Instructions />
+                            <AdjustSettings />
+                            <RequestsList />
+                        </div>
+                    </div>
+                    <Footer />{' '}
                 </div>
             </AddWidgetDataContext>
         </Main>
+    );
+}
+
+function Footer() {
+    const classes = [
+        'grow-0',
+        'w-full',
+        'border-t md:border-t-0 dark:border-white/20',
+        'md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient',
+        'bg-white dark:bg-neutral-800 md:!bg-transparent dark:md:bg-vert-dark-gradient pt-2',
+    ].join(' ');
+    return (
+        <div className={classes}>
+            <TextInputForm />
+        </div>
     );
 }
