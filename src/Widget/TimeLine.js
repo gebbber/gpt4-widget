5f739ac847c6bf088517a45336795cfde495ebd5
Greg Brandon <gebbber@gmail.com>
2023-04-02 16:21:19 -0600

    Added ticks to ends of timeline

--- src/Widget/TimeLine.js
+++ src/Widget/TimeLine.js
@@ -2,9 +2,7 @@ export default function TimeLine({ tlRef, children }) {
     const timelineColors = 'border-black/30 dark:border-white/20';
 
     return (
-        <div
-            className={`flex-1 min-h-full relative border-l border-r ${timelineColors}`}
-            ref={tlRef}>
+        <div className={`flex-1 h-2 relative border-l border-r ${timelineColors}`} ref={tlRef}>
             <div className={`absolute top-1/2 border-t inset-x-0 ${timelineColors}`}></div>
             <div>{children}</div>
         </div>
